6783a2eeeb7b7af7ffd140b59a91b28eeba68e70
Chenchunaidu <maddinenichenchunaidu@gmail.com>
2022-10-01 18:40:52 +0530

    feat: moved scrap.mjs to src/queues folder

--- src/queues/scrap-and-update-recommendation/index.mjs
+++ src/queues/scrap-and-update-recommendation/index.mjs
@@ -1,5 +1,5 @@
 // learn more about event functions here: https://arc.codes/events
-import { scrapAndUpdateMeta } from "../../lib/scrap.mjs";
+import { scrapAndUpdateMeta } from "./scrap.mjs";
 export async function handler(event) {
   await Promise.all(
     event?.Records.map(async (record) => {
