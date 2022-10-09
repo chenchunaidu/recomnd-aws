b16b6f4a2a68bd2f8dd5c829f9ff3f5404584b8b
Chenchunaidu <maddinenichenchunaidu@gmail.com>
2022-10-09 20:28:46 +0530

    fix: added logout button

--- app/components/common/header.tsx
+++ app/components/common/header.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Image from "../common/image";
 import logo from "~/assets/images/recomnd-logo-with-text.svg";
+import Button from "./button";
 
 interface NavLinkProps {
   children: ReactNode;
@@ -25,10 +26,14 @@ const Header: FC<HeaderProps> = ({ userId }) => {
   return (
     <div className="flex w-full items-center justify-between border bg-white p-4">
       <Image src={logo} className="h-10" />
-      <div className="flex space-x-4 md:mr-16 md:space-x-8">
+      <div className="flex items-center space-x-4 md:mr-16 md:space-x-8">
         <NavLink to="/home">Home</NavLink>
         <NavLink to="/home/groups">Groups</NavLink>
-        <NavLink to="/account">Account</NavLink>
+        <form method="post" action="/logout">
+          <Button variant="link" type="submit" className="px-0">
+            Logout
+          </Button>
+        </form>
       </div>
     </div>
   );
--- app/root.tsx
+++ app/root.tsx
@@ -15,7 +15,6 @@ import {
 
 import tailwindStylesheetUrl from "./styles/tailwind.css";
 import rootStyles from "./root.css";
-import { getUser } from "./session.server";
 
 export const links: LinksFunction = () => {
   return [
@@ -31,16 +30,6 @@ export const meta: MetaFunction = () => ({
   title: "Recomnd",
   viewport: "width=device-width,initial-scale=1",
 });
-
-type LoaderData = {
-  user: Awaited<ReturnType<typeof getUser>>;
-};
-
-export const loader: LoaderFunction = async ({ request }) => {
-  return json<LoaderData>({
-    user: await getUser(request),
-  });
-};
 
 export default function App() {
   return (
