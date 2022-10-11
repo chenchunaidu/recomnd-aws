4efaa4b4280a5703fca957398061179f2c29f1e3
Chenchunaidu <maddinenichenchunaidu@gmail.com>
2022-10-11 23:26:49 +0530

    feat: added login banner

--- app/routes/users/$userId.tsx
+++ app/routes/users/$userId.tsx
@@ -2,17 +2,21 @@ import type { LoaderFunction } from "@remix-run/node";
 import { Outlet, useLoaderData } from "@remix-run/react";
 import Container from "~/components/common/container";
 import Fullscreen from "~/components/common/full-screen";
+import LoginBanner from "~/components/common/login-banner";
 import Profile from "~/components/user/profile";
+import { isAuthenticated } from "~/lib/auth/auth";
 import { getUserById } from "~/models/user.server";
 
-export const loader: LoaderFunction = async ({ params }) => {
+export const loader: LoaderFunction = async ({ request, params }) => {
+  const authenticated = await isAuthenticated(request);
+  console.log(authenticated);
   if (!params.userId) return { groups: [] };
   const user = await getUserById(params.userId);
-  return { user };
+  return { user, authenticated };
 };
 
 export default function UserPage() {
-  const { user } = useLoaderData();
+  const { user, authenticated } = useLoaderData();
 
   return (
     <Fullscreen className="flex flex-col items-center">
@@ -20,6 +24,13 @@ export default function UserPage() {
         <Profile user={user} />
         <Outlet />
       </Container>
+      {!authenticated ? (
+        <div className="fixed bottom-0 flex w-full justify-center bg-violet-700  p-4">
+          <LoginBanner />
+        </div>
+      ) : (
+        <div></div>
+      )}
     </Fullscreen>
   );
 }
